b136de3ae04691212ed4ee9ac60376e93c46d668
SShatzkin <sshatzkin@uchicago.edu>
2022-04-03 11:44:55 -0700

    Added level switching

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -20,6 +22,20 @@ public class GameManager : MonoBehaviour
         {
             // We want to switch the camera position
             camPositions.NextPosition();
+        }
+        
+        // Go to space when you hit the 1 key.
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("EarthSimpleScene1");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+          SceneManager.LoadScene("NewYork");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+          SceneManager.LoadScene("KinectHolographic");
         }
     }
 }
